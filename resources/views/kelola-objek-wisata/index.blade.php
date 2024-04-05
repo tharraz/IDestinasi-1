47b29eb18c2515b01fd0c81c313d55a985d4a8b1
adibyrmn <adib.pbnc@gmail.com>
2024-04-05 21:56:42 +0700

    ID-22 (In progress)

--- resources/views/kelola-objek-wisata/index.blade.php
+++ resources/views/kelola-objek-wisata/index.blade.php
@@ -2,9 +2,20 @@
 
 @section('content')
     <div class="container">
-        <h2>Daftar Aktivitas Mingguan</h2>
-        <a href="{{ route('aktifitasmingguan.create') }}" class="btn btn-success">Tambah Kegiatan</a>
+        <h2>Objek Wisatamu</h2>
+        <h6>Lihat objek wiata yang sudah kamu upload</h6>
+        <a href="{{ route('kelola-objek-wisata.create') }}" class="btn btn-success">Daftar Sekarang</a>
         
-        
+        <div class="row">
+            <div class="col-md-4">
+                <img src="/img/Derawan.png" class="img-fluid rounded mb-3" alt="...">
+            </div>
+            <div class="col-md-4">
+                <img src="/img/lombok.png" class="img-fluid rounded mb-3" alt="...">
+            </div>
+            <div class="col-md-4">
+                <img src="/img/ntt.png" class="img-fluid rounded mb-3" alt="...">
+            </div>
+        </div>
     </div>
 @endsection
