824b521bf0d60d34dc5027099e324b8d180885e2
tharraz <tharraze@gmail.com>
2024-04-07 23:52:23 +0700

    maintain regist 2

--- app/Http/Controllers/administratorController.php
+++ app/Http/Controllers/administratorController.php
@@ -19,6 +19,8 @@ class administratorController extends Controller
         $update_verifikasi = 
     }
 
+}
+
     
 
   
---
+++ resources/layout/login.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+  <h1>Selamat datang</h1>
+  <p>Lanjutkan dengan Google atau Masukkan Detail Login</p>
+  <form>
+    <label for="email">Alamat email</label><br>
+    <input type="email" id="email" name="email" required><br>
+    <label for="password">Kata sandi</label><br>
+    <input type="password" id="password" name="password" required><br>
+    <input type="submit" value="Masuk">
+  </form>
+  <p>Belum punya akun? <a href="signup.html">Buat Akun</a></p>
+  <p>atau <a href="login.html">G</a></p>
+</body>
+</html>
