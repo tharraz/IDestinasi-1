2f0f13963470ecaf14c67b92a367664b12604d28
adibyrmn <adib.pbnc@gmail.com>
2024-04-19 18:08:37 +0700

    ID-22

--- resources/views/kelola-objek-wisata/index.blade.php
+++ resources/views/kelola-objek-wisata/index.blade.php
@@ -7,15 +7,21 @@
         <a href="{{ route('kelola-objek-wisata.create') }}" class="btn btn-success">Daftar Sekarang</a>
         
         <div class="row">
-            <div class="col-md-4">
-                <img src="/img/Derawan.png" class="img-fluid rounded mb-3" alt="...">
-            </div>
-            <div class="col-md-4">
-                <img src="/img/lombok.png" class="img-fluid rounded mb-3" alt="...">
-            </div>
-            <div class="col-md-4">
-                <img src="/img/ntt.png" class="img-fluid rounded mb-3" alt="...">
-            </div>
+            @foreach($pengaturanObjekWisatas as $pengaturanObjekWisata)
+                <div class="col-md-4">
+                    <img src="{{ asset('storage/post-images/' . $pengaturanObjekWisata->foto) }}" class="img-fluid rounded mb-3" alt="{{ $pengaturanObjekWisata->nama }}">
+                    <h4>{{ $pengaturanObjekWisata->nama }}</h4>
+                    <h4>{{ $pengaturanObjekWisata->Htiket }}</h4>
+                    <div class="btn-group" role="group" aria-label="Basic example">
+                        <a href="{{ route('kelola-objek-wisata.edit', $pengaturanObjekWisata->id) }}" class="btn btn-primary">Edit</a>
+                        <form action="{{ route('kelola-objek-wisata.destroy', $pengaturanObjekWisata->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Hapus</button>
+                        </form>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection
